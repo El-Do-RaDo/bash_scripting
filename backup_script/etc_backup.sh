0217b38c0b29f033dc2cd805c471569c472f8b0a
El-Do-RaDo <manjaniket@gmail.com>
2021-01-18 18:19:19 +0530

    adding description in etc file...

--- backup_script/etc_backup.sh
+++ backup_script/etc_backup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-#here we give the directory name where we want to create our back file so we replace /home/aniket/ with the directory name that we want
+#Here we give the directory name where we want to create our back file so we replace /home/aniket/ with the directory name that we want
 sudo mkdir /home/aniket/backups && cd "/home/aniket/backups/" 
+#Here we are creating the back up of /etc directory with all the data lib content of etc and creating a zip file of that directory with particular date on the file name. 
 sudo zip -r "etc-$(date +"%Y-%m-%d").zip" /etc/
