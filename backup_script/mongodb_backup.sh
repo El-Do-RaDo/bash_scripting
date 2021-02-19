da48e6ef8837465b35973df71f11c7a954172b73
Aniket Sangohi <manjaniket@gmail.com>
2021-02-19 15:53:57 +0530

    adding...

--- backup_script/mongodb_backup.sh
+++ backup_script/mongodb_backup.sh
@@ -7,10 +7,12 @@ for db in `cat db.txt`; do
 mongodump  --db $db --out $HOME/backup/$(date +"%d-%m-%Y")/
 done
 
+
+#this part of the script is to upload ur backup to S3 bucket but you can avoid it and replace it with you appropiate
 zip -r $HOME/backup/$(date +"%d-%m-%Y").zip $HOME/backup/$(date +"%d-%m-%Y")
 aws s3 cp $HOME/backup/$(date +"%d-%m-%Y").zip s3://<bucket_name>/db_backup/
 
-
+#this  will remove all other things from the local system
 rm -rf $HOME/db.txt
 rm -rf $HOME/backup/*
 
